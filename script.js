1230e8e98b6847a5d70cc6ef3959287709d93641
Sebastian W <sebastian.walacik94@gmail.com>
2023-01-07 17:58:47 +0100

    create measument class and its constructor + app class

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const measuSubmit = document.querySelector("#meassubmit");
+
 const navBtnCnt = document.querySelector(".nav");
 let clicked;
 navBtnCnt.addEventListener("click", (e) => {
@@ -14,3 +16,20 @@ navBtnCnt.addEventListener("click", (e) => {
     .querySelector(`.section--${clicked.dataset.tab}`)
     .classList.add("section--active");
 });
+
+class Measurement {
+  id = (Date.now() + "").slice(-10);
+
+  constructor(systolic, diastolic, puls, date, time) {
+    this.systolic = systolic;
+    this.diastolic = diastolic;
+    this.puls = puls;
+    this.date = date;
+    this.time = time;
+  }
+}
+
+class app {}
+
+const test = new Measurement();
+console.log(test);
